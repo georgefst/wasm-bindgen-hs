c421171f969acbc528102f00a391d488ba9633c8
George Thomas <georgefsthomas@gmail.com>
2026-06-30 15:13:40 +0100

    Bump all flake inputs and use HLS master for splice plugin

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     haskell-nix.url = "github:input-output-hk/haskell.nix";
     nixpkgs.follows = "haskell-nix/nixpkgs-2511";
     flake-utils.url = "github:numtide/flake-utils";
-    hls-2-13 = { url = "github:haskell/haskell-language-server/2.13.0.0"; flake = false; };
+    hls-master = { url = "github:haskell/haskell-language-server/master"; flake = false; };
     ghc-wasm-meta.url = "gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org";
     self.submodules = true;
   };
@@ -21,12 +21,7 @@
                 evalSystem = "x86_64-linux";
                 shell.nativeBuildInputs = [ inputs.ghc-wasm-meta.packages.${system}.default ];
                 shell.tools.cabal = "latest";
-                shell.tools.haskell-language-server = {
-                  src = inputs.hls-2-13;
-                  sha256map = {
-                    "https://github.com/snowleopard/alga"."d4e43fb42db05413459fb2df493361d5a666588a" = "0s1mlnl64wj7pkg3iipv5bb4syy3bhxwqzqv93zqlvkyfn64015i";
-                  };
-                };
+                shell.tools.haskell-language-server.src = inputs.hls-master;
                 shell.withHoogle = false;
               };
           })
